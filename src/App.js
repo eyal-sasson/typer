4d911beed1d79a0acd7db0589459f585fc744c6e
Eyal Sasson <eyals180@gmail.com>
2022-11-11 20:41:39 +0200

    Adding basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PLACEHOLDER = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Typer />
+      </div>
+    );
+  }
 }
 
-export default App;
+class Typer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: this.getText(),
+            typed: "",
+        };
+    }
+    componentDidMount() {
+        document.addEventListener("keydown", ev => this.onType(ev));
+    }
+    getText() {
+        return PLACEHOLDER;
+    }
+    onType(ev) {
+        const { text, typed } = this.state;
+        if (ev.key === text[0]) {
+            this.setState({
+                text: text.slice(1),
+                typed: typed + text[0],
+            });
+        }
+    }
+    render() {
+        return (
+            <div className="Typer">
+                <pre id="text"><span id="typed">{this.state.typed}</span>{this.state.text}</pre>
+            </div>
+        );
+    }
+}
+
